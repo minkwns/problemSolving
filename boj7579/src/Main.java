f32484677051866fd4b9e8766f284b38d63c8eec
jinwoo-Bae <wlsdn3578@gmail.com>
2021-09-10 00:55:10 +0900

    Fix: fix the wrong commit

--- boj7579/src/Main.java
+++ boj7579/src/Main.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int numOfApp = Integer.parseInt(st.nextToken());
+
+        int need = Integer.parseInt(st.nextToken());
+
+        int[] memory = new int[numOfApp];
+
+        int[] expense = new int[numOfApp];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < numOfApp; i++) {
+            memory[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < numOfApp; i++) {
+            expense[i] = Integer.parseInt(st.nextToken());
+        }
+
+    }
 }
